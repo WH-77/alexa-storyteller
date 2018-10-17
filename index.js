47e57b0960b8c11af1e40e98d01a3bb1afc20c40
William Hou <hou.w.77@gmail.com>
2018-10-17 14:58:59 -0400

    Refactor skill as audio player skill.

--- index.js
+++ index.js
@@ -13,10 +13,13 @@ const WHERE_THE_WILD_THINGS_ARE = [
 ];
 
 const STORIES = {
-  "1": WHERE_THE_WILD_THINGS_ARE,
+  '1': WHERE_THE_WILD_THINGS_ARE,
 };
 
 const STORY_IDS = Object.keys(STORIES);
+
+const REPLACE_ALL = 'REPLACE_ALL';
+const ENQUEUE = 'ENQUEUE';
 
 // ============================================================================
 // Initialization:
@@ -25,89 +28,87 @@ const STORY_IDS = Object.keys(STORIES);
 const alexaApp = new Alexa.app('storyteller');
 
 // ============================================================================
-// Intent handlers:
-
-// ================
-// Built in:
-
-// Amazon built-in intents do not require custom slot/utterence schemas:
-// - Schema don't actually do anything unless using ASK CLI.
-const DEFAULT_SCHEMA = {
-  slots: {},
-  utterances: [],
-};
+// Intents:
 
 /** Session initialization. */
-alexaApp.launch((request, response) => {
-  console.log('launched');
-  response
-    .say('What story would you like me to tell?')
-    .shouldEndSession(false);
-});
+alexaApp.launch(askForStoryHandler);
 
-alexaApp.intent('AMAZON.NextIntent',
-  (request, response) => {
-    console.log(request);
-    readStory(request, response);
-  }
-);
+// Audio Player required intents:
+alexaApp.intent('AMAZON.PauseIntent', pauseHandler);
+// TODO - Use database to handle resumption:
+alexaApp.intent('AMAZON.ResumeIntent', askForStoryHandler);
+alexaApp.intent('AMAZON.CancelIntent', cancelHandler);
+alexaApp.intent('AMAZON.LoopOffIntent', unsupportedHandler);
+alexaApp.intent('AMAZON.LoopOnIntent', unsupportedHandler);
+alexaApp.intent('AMAZON.NextIntent', continueStoryHandler);
+alexaApp.intent('AMAZON.PreviousIntent', unsupportedHandler);
+alexaApp.intent('AMAZON.RepeatIntent', unsupportedHandler);
+alexaApp.intent('AMAZON.ShuffleOffIntent', unsupportedHandler);
+alexaApp.intent('AMAZON.ShuffleOnIntent', unsupportedHandler);
+alexaApp.intent('AMAZON.StartOverIntent', unsupportedHandler);
 
-/** Continue the story on any further interaction. */
-alexaApp.intent('AMAZON.FallbackIntent',
-  (request, response) => {
-    console.log(request);
-    readStory(request, response);
-  }
-);
+// Other built-in intents:
+alexaApp.intent('AMAZON.FallbackIntent', continueStoryHandler);
 
-// ================
-// Custom:
-
-alexaApp.intent('BeginStoryIntent',
-  (request, response) => {
-    const slot = request.slots['Story'];
-
-    let story = slot.resolution().first().id;
-    let value = request.slot('Story');
-
-    // Random story:
-    if (story < 0) {
-      story = STORY_IDS[Math.floor(Math.random() * STORY_IDS.length)];
-      value = 'a story';
-    }
-
-    console.log(`Beginning story: ${story}`);
-
-    // Initialize the current story session:
-    const session = request.getSession();
-    session.set('story', story);
-    session.set('index', 0);
-
-    response.say(`All right, I'll read ${value}.`);
-
-    readStory(request, response);
-  }
-);
-
-alexaApp.intent('ContinueStoryIntent',
-  (request, response) => {
-    readStory(request, response);
-  }
-);
+// Custom intents:
+alexaApp.intent('BeginStoryIntent', beginStoryHandler);
+alexaApp.intent('ContinueStoryIntent', continueStoryHandler);
 
 // ============================================================================
-// Helper functions:
+// Intent handlers:
 
 /**
  * @param {Alexa.request} request 
  * @param {Alexa.response} response
 */
-function readStory(request, response) {
+function askForStoryHandler(request, response) {
+  console.log(request);
+
+  response
+    .say('What story would you like me to tell?')
+    .shouldEndSession(false);
+}
+
+/**
+ * @param {Alexa.request} request 
+ * @param {Alexa.response} response
+*/
+function beginStoryHandler(request, response) {
+  console.log(request);
+
+  const slot = request.slots['Story'];
+
+  let story = slot.resolution().first().id;
+  let value = request.slot('Story');
+
+  // Random story:
+  if (story < 0) {
+    story = STORY_IDS[Math.floor(Math.random() * STORY_IDS.length)];
+    value = 'a story';
+  }
+
+  console.log(`Beginning story: ${story}`);
+
+  // Initialize the current story session:
+  const session = request.getSession();
+  session.set('story', story);
+  session.set('index', 0);
+
+  response.say(`All right, I'll read ${value}.`);
+
+  continueStoryHandler(request, response);
+}
+
+/**
+ * @param {Alexa.request} request 
+ * @param {Alexa.response} response
+*/
+function continueStoryHandler(request, response) {
+  console.log(request);
+
   const session = request.getSession();
   const story = session.get('story');
   const index = session.get('index');
-
-  const builder = new SSMLBuilder();
 
   // Light error checking:
   const script = STORIES[story];
@@ -121,13 +122,109 @@ function readStory(request, response) {
     return;
   }
 
-  // Construct audio SSML for Alexa:
-  builder.audio(script[index]);
+  // Play audio of the next part of the script:
+  const stream = {
+    url: script[index],
+    token: encodeState({ story, index }),
+    offsetInMilliseconds: 0,
+  };
 
-  response.say(builder.ssml(true)).shouldEndSession(false);
+  response.audioPlayerPlayStream(REPLACE_ALL, stream);
 
   // Update the session:
   session.set('index', index + 1);
+}
+
+/**
+ * @param {Alexa.request} request 
+ * @param {Alexa.response} response
+*/
+function pauseHandler(request, response) {
+  response.audioPlayerStop();
+  response.say("All right, feel free to restart the story whenever you'd like.");
+}
+
+/**
+ * @param {Alexa.request} request 
+ * @param {Alexa.response} response
+*/
+function cancelHandler(request, response) {
+  response.audioPlayerClearQueue();
+  response.say("Got it, thanks for listening.");
+}
+
+/**
+ * @param {Alexa.request} request 
+ * @param {Alexa.response} response
+*/
+function unsupportedHandler(request, response) {
+  response.say("Sorry, I can't do that yet.");
+}
+
+/**
+ * @param {Alexa.request} request 
+ * @param {Alexa.response} response
+*/
+function nullHandler(request, response) {
+  console.log(request);
+}
+
+// ============================================================================
+// Audio player requests:
+
+alexaApp.audioPlayer('PlaybackStarted', nullHandler);
+alexaApp.audioPlayer('PlaybackFinished', nullHandler);
+alexaApp.audioPlayer('PlaybackStopped', nullHandler);
+alexaApp.audioPlayer('PlaybackNearlyFinished', audioReplayHandler);
+alexaApp.audioPlayer('PlaybackFailed', nullHandler);
+
+/**
+ * @param {Alexa.request} request 
+ * @param {Alexa.response} response
+*/
+function audioReplayHandler(request, response) {
+  const { story, index, token } = getStateFromAudioRequest(request);
+
+  const stream = {
+    url: STORIES[story][index],
+    token,
+    expectedPreviousToken: token,
+    offsetInMilliseconds: 0,
+  }
+
+  response.audioPlayerPlayStream(ENQUEUE, stream);
+}
+
+/**
+ * @param {Alexa.request} request
+*/
+function getStateFromAudioRequest(request) {
+  if (!request.isAudioPlayer) {
+    console.error('Expected Audio Player request.');
+  }
+
+  const { token } = request.data;
+  console.log(token);
+
+  const fields = token.split('-');
+  const story = fields[0];
+  const index = fields[1];
+
+  return { story, index, token };
+}
+
+function encodeState({ story, index }) {
+  return `${story}-${index}`;
+}
+
+// ============================================================================
+// Helper functions:
+
+// Credit - https://gist.github.com/jed/982883:
+function uuid(a) {
+  return a
+    ? (a^Math.random()*16>>a/4).toString(16)
+    : ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, b);
 }
 
 // ============================================================================
